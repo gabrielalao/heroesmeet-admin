f19d1efaa27a53d27c2453f66f4d872b8a88b219
rishiankush <ankushrishi92@gmail.com>
2020-02-07 09:18:12 +0530

    env config

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ var cors = require('cors')
 
 var app = express();
 
-require('dotenv').config({ path: process.env });
-
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,7 +54,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') == 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
